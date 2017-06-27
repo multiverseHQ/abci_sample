bd229c56ae59af9dc8a87234d74a53d4bed413cc
Antoine <antoine@cercle.co>
2017-06-27 13:40:46 +0200

    improve our sample app

--- abci_sample.go
+++ abci_sample.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"os"
+	"fmt"
+
+	"github.com/MultiverseHQ/abci_sample/counter"
+	"github.com/tendermint/abci/server"
+	cmn "github.com/tendermint/tmlibs/common"
+	"github.com/tendermint/tmlibs/log"
+)
 
 func main() {
-	fmt.Printf("Hello, world 1.\n")
+	fmt.Printf("Welcome to Multiverse\n")
+	fmt.Printf("<3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3\n")
+	fmt.Printf("This is the ABCi Developper Application Example.\n")
+	fmt.Printf("<3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3\n")
+
+	addrPtr := flag.String("addr", "tcp://0.0.0.0:46658", "Listen address")
+	abciPtr := flag.String("abci", "socket", "ABCI server: socket | grpc")
+	serialPtr := flag.Bool("serial", false, "Enforce incrementing (serial) txs")
+	flag.Parse()
+	app := counter.NewCounterApplication(*serialPtr)
+
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+
+	// Start the listener
+	srv, err := server.NewServer(*addrPtr, *abciPtr, app)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	srv.SetLogger(logger.With("module", "abci-server"))
+	if _, err := srv.Start(); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// Wait forever
+	cmn.TrapSignal(func() {
+		// Cleanup
+		srv.Stop()
+	})
+
 }
